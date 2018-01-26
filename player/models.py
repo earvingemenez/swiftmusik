e72b75f5cf6d71db7c2b7f9e3e2b756dacca55a9
Kevin Kalis <kevinpaulkalis@gmail.com>
2018-01-26 13:48:16 +0800

    Add player app

---
+++ player/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Video(models.Model):
+    """Videos that are to be added by the users to the playlist
+    """
+    QUEUED = 'queued'
+    PLAYING = 'playing'
+    FINISHED = 'finished'
+    CANCELED = 'canceled'
+
+    STATUS_CHOICES = (
+        (QUEUED, 'Queued'),
+        (PLAYING, 'Playing'),
+        (FINISHED, 'Finished'),
+        (CANCELED, 'Canceled')
+    )
+
+    url = models.CharField(max_length=255)
+    parsed_id = models.CharField(max_length=255)
+    parsed_title = models.CharField(max_length=255, default='No Title')
+
+    status = models.CharField(choices=STATUS_CHOICES, default=QUEUED)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.parsed_title, self.url)
