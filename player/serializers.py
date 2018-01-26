fc57f19ab614417a91321138f0ca379e2b02bcf4
Kevin Kalis <kevinpaulkalis@gmail.com>
2018-01-26 17:58:53 +0800

    Get ID from url

--- player/serializers.py
+++ player/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 
 from player.models import Video
@@ -8,3 +9,17 @@ class VideoSerializer(serializers.ModelSerializer):
         model = Video
         fields = ('url', 'parsed_id', 'parsed_title', 'status', 'date_added', )
         read_only_fields = ('parsed_id', 'parsed_title', 'status', 'date_added', )
+
+    def create(self, validated_data):
+        pattern = '((?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)'
+        compiled_pattern = re.compile(pattern)
+
+        url = validated_data.get('url', None)
+        if url:
+            # Parse URL for video ID
+            vid_id = compiled_pattern.search(url)
+            validated_data['parsed_id'] = vid_id.group(0)
+
+        video = Video.objects.create(**validated_data)
+
+        return validated_data
