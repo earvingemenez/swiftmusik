8b7c49418aa88d5c2ac2fb04e92fedd6738f5d40
Kevin Kalis <kevinpaulkalis@gmail.com>
2018-02-01 11:46:59 +0800

    Add postgres

--- swiftmusik/settings.py
+++ swiftmusik/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'swiftmusik.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('SK_DB_NAME', ''),
+        'USER': os.environ.get('SK_DB_USER', ''),
+        'PASSWORD': os.environ.get('SK_DB_PASS', ''),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
